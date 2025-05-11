b32414046531e17b4c89b7290118b56549050455
Andrew Palmer <andrew.pa@outlook.com>
2025-05-10 18:49:04 -0700

    add integration tests

--- services/check-syscalls/src/read_env_value.rs
+++ services/check-syscalls/src/read_env_value.rs
@@ -5,6 +5,7 @@ use crate::Testable;
 fn process_id() {
     assert_eq!(read_env_value(EnvironmentValue::CurrentProcessId), 1);
 }
+
 fn thread_id() {
     // this value depends on the number of processors since each processor gets an idle thread
     assert!(read_env_value(EnvironmentValue::CurrentThreadId) >= 1);
@@ -18,6 +19,16 @@ fn page_size() {
 fn invalid_env_value() {
     let bad_env_value = unsafe { core::mem::transmute::<usize, EnvironmentValue>(99usize) };
     assert_eq!(read_env_value(bad_env_value), 0);
+}
+
+fn supervisor_id() {
+    // we are the root process
+    assert_eq!(read_env_value(EnvironmentValue::CurrentSupervisorQueueId), 0);
+}
+
+fn registry_id() {
+    // we are the root process
+    assert_eq!(read_env_value(EnvironmentValue::CurrentRegistryQueueId), 0);
 }
 
 pub const TESTS: (&str, &[&dyn Testable]) = (
