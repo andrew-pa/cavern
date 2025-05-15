cdb800b5acefdba4c1efaae6da7a6341f72c76c2
Andrew Palmer <andrew.pa@outlook.com>
2025-05-14 21:00:55 -0700

    clean up, reintegrate final syscall test to main mod

--- kernel_core/src/process/system_calls/mod.rs
+++ kernel_core/src/process/system_calls/mod.rs
@@ -3,25 +3,19 @@
 
 use alloc::{string::String, sync::Arc};
 use bytemuck::Contiguous;
-use kernel_api::{
-    flags::{ExitNotificationSubscriptionFlags, FreeMessageFlags},
-    CallNumber, ErrorCode, ExitReason, Message, ProcessId, QueueId, ThreadId,
-};
-use log::{debug, error, warn};
-use snafu::{ensure, OptionExt, ResultExt, Snafu};
+use kernel_api::{CallNumber, ErrorCode, ExitReason, QueueId};
+use log::{error, warn};
+use snafu::{ensure, OptionExt, Snafu};
 
 use crate::{
-    memory::{
-        active_user_space_tables::{ActiveUserSpaceTables, ActiveUserSpaceTablesChecker},
-        PageAllocator, VirtualAddress, VirtualPointer,
-    },
+    memory::{active_user_space_tables::ActiveUserSpaceTables, PageAllocator},
     process::{kill_thread_entirely, MessageQueue},
 };
 
 use super::{
     queue::QueueManager,
     thread::{Registers, ThreadManager},
-    ManagerError, Process, ProcessManager, SharedBufferId, Thread, TransferError,
+    ManagerError, Process, ProcessManager, Thread, TransferError,
 };
 
 /// Errors that can arise during a system call.
@@ -336,4 +330,64 @@ impl<'pa, 'm, PA: PageAllocator, PM: ProcessManager, TM: ThreadManager, QM: Queu
 }
 
 #[cfg(test)]
-mod tests;
+mod tests {
+    use core::num::NonZeroU32;
+    use std::assert_matches::assert_matches;
+
+    use crate::{
+        memory::{
+            active_user_space_tables::MockActiveUserSpaceTables, MockPageAllocator, VirtualAddress,
+        },
+        process::{queue::MockQueueManager, thread::MockThreadManager, MockProcessManager},
+    };
+
+    use super::*;
+
+    pub fn fake_thread() -> Arc<Thread> {
+        Arc::new(Thread::new(
+            NonZeroU32::new(777).unwrap(),
+            None,
+            crate::process::thread::State::Running,
+            crate::process::thread::ProcessorState::new_for_user_thread(
+                VirtualAddress::null(),
+                VirtualAddress::null(),
+                0,
+            ),
+            (VirtualAddress::null(), 0),
+        ))
+    }
+
+    #[test]
+    fn invalid_syscall_number() {
+        let pa = MockPageAllocator::new();
+        let pm = MockProcessManager::new();
+        let mut tm = MockThreadManager::new();
+        let qm = MockQueueManager::new();
+
+        let thread = fake_thread();
+
+        // invalid syscall number -> thread fault
+        let thread2 = thread.clone();
+        tm.expect_exit_thread()
+            .once()
+            .withf(move |t, r| t.id == thread2.id && *r == ExitReason::invalid_syscall())
+            .returning(|_, _| false);
+
+        let policy = SystemCalls::new(&pa, &pm, &tm, &qm);
+
+        let usm = MockActiveUserSpaceTables::new();
+
+        let registers = Registers::default();
+
+        let system_call_number_that_is_invalid = 1;
+        assert_matches!(
+            policy.dispatch_system_call(
+                system_call_number_that_is_invalid,
+                &thread,
+                &registers,
+                &usm
+            ),
+            Ok(SysCallEffect::ScheduleNextThread)
+        );
+    }
+}
